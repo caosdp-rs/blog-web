8af76794a1430cf66ea71d4796cf57eb7c1c3602
caosdp-rs <caosdp@gmail.com>
2023-01-07 03:03:41 -0300

    Criando as rotas e o controller inicial de usuário

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    //Register User
+    public function register(Request $request)
+    {
+        //validate fields
+        $attrs = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        //create user
+        $user = User::create([
+            'name' => $attrs['name'],
+            'email' => $attrs['email'],
+            'password' => bcrypt($attrs['password']),
+        ]);
+
+        //return user & token in response
+        return response([
+            'user' => $user, 
+            'token' => $user->createToken('secret')->plainTextToken
+        ]);
+    }
+    //Login User
+    public function login(Request $request)
+    {
+        
+        //validate fields
+        $attrs = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        //attempt login
+        if(!Auth::attempt($attrs))
+        {
+            return response([
+                'message'=>'Usuário inválido.'
+            ],403);
+        }
+
+        //return user & token in response
+        return response([
+            'user' => auth()->user(),
+            'token' => auth()->user()->createToken('secret')->plainTextToken,
+        ],200);
+    }
+
+    //logout user
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return response([
+            'message'=>'Logout realizado!'
+        ],200);
+    }
+
+    //get user details
+    public function user()
+    {
+        return response([
+            'user'=> auth()->user()
+        ],200);
+    }
+}
